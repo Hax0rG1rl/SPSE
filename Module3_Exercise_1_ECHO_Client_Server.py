08a88ec0975907d8770da77ab0f6c3a335f06d2e
Johnny STANESCU <johnny.stanescu@pentestconsultancy.com>
2013-03-13 14:00:36 +0200

    Update Module3_Exercise_1_ECHO_Client_Server.py

--- Module3_Exercise_1_ECHO_Client_Server.py
+++ Module3_Exercise_1_ECHO_Client_Server.py
@@ -3,6 +3,7 @@
 # Simple UDP ECHO Client-Server Python2.7.3 script - by JohnnyAMD - 03/12/2013
 
 import socket
+import random
 
 # Socket opened for TCP protocol
 tcpsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,17 +20,29 @@ tcpsocket.bind(('0.0.0.0', 2345))
 
 #How many concurrent connections this can handle
 tcpsocket.listen(1)
+
+# Setting up a random number and display the value
+x = random.randrange(100)
+print 'Value to be guessed is: %d'%x 
+
+# Waiting for a client to connect
 print 'waiting for a client.... '
 (client, (ip,port)) = tcpsocket.accept()
 print 'Receiging info from:.. ', ip
 print 'starting ECHO output '
 
-#Print the received data from the client
+#Print the received data from the client and check to see if he guessed the value
 while 1:
-  data = client.recv(128)
+	data = client.recv(128)
 	print 'Client sent: ', data
 	client.send(data)
+	if int(data) == x:
+		print "You've got it. Some magic inside maybe."
+		break
+	else: 
+		print "He didn't pointed the correct value though"
+
 
 # Byffer has been reach out, clossing the channel
-print 'closing comm channel...'
+print "Closing comm channel."
 client.close()

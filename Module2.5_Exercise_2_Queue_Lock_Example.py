aa890deeef839783e3d1270036bab32085391cf7
Johnny STANESCU <johnny.stanescu@pentestconsultancy.com>
2013-03-14 16:00:30 +0200

    Update Module2.5_Exercise_2_Queue_Lock_Example.py

--- Module2.5_Exercise_2_Queue_Lock_Example.py
+++ Module2.5_Exercise_2_Queue_Lock_Example.py
@@ -1,34 +1,45 @@
 # !/usr/bin/env python
 
+# Simple Queue Lock mechanism example script - Python2.7.3 - by JohnnyAMD - 03/07/2013
+
 import threading
 import Queue
 import time
 import ftplib
 
-
+# opening the ftp file for reading the contents
 print "\nOpening for reading the ftp list.\n" 
 lines = []
 text_file = open("anonymous_ftp_sites.txt", "r")
 lines = text_file.readlines()
 print lines
+
+# Displaying some info about what's inside
 print 'The file contains: %d'%len(lines) + ' ftp sites'
 text_file.close()
 
+# using the threading class
 class WorkerThread(threading.Thread):
 
   def __init__(self, queue):
 		threading.Thread.__init__(self)
 		self.queue = queue
 
+# defining the constructor
 	def run(self):
 		print "In WorkerThread"
 		while True:                        
 			try:
                                 counter = self.queue.get()
                                 for line in lines:     
+# doing some checking for nasty things as \n and like
                                     if line == '\n':
                                         break
                                     else:
+
+# here I'm formatting the read lines in such way that they contain only the host name and nothing else. 
+# Some restrictions are applied thought as ftp:// is considered not being present as prefix of the listed targets
+
                                         line = line.rstrip('\n')
                                         print 'The target name has been checked: ' + line  + '\n'
                                         ftp = ftplib.FTP(line)
@@ -42,6 +53,9 @@ class WorkerThread(threading.Thread):
 				time.sleep(counter)
 				print "Finished sleeping for %d seconds"%counter
 				self.queue.task_done()
+				
+# trying to catch as many possible exceptions could appear and handle them accordingly
+
 			except NameError, detail:
 				print 'Something is wrong', detail
 				time.sleep(3)
@@ -63,8 +77,12 @@ class WorkerThread(threading.Thread):
 queue = Queue.Queue()
 lock = threading.Lock()
 
+# populating queue
+
 for j in lines:
 	queue.put(j)
+	
+# creating threads
 
 for i in range(5):
 	print "Creating a WorkerThread : %d"%i	
